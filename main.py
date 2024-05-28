48943206b4a30a2ff4a6aee5620fb5360a53a3d2
ebarroso2214 <ebarroso2214@gmail.com>
2024-05-28 16:41:43 -0400

    mistake

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+from pytesseract import pytesseract
+import numpy as np
+
+from PIL import Image
+
+camera = cv2.VideoCapture(0)
+translated_text = None
+
+while True:
+    _,image=camera.read()
+    cv2.imshow('text detection',image)
+    if cv2.waitKey(1)&0xFF==ord('s'):
+        cv2.imwrite('test.jpg',image)
+        break
+
+camera.release()
+cv2.destroyAllWindows
+#usr/local/bin/tesseract.exe
+
+def end_product():
+    path_to_tesseract =r'/usr/local/bin/tesseract' ##For this to work, need to add tesseract to PATH
+    Imagepath = 'test.jpg'
+    pytesseract.tesseract_cmd=path_to_tesseract
+    text= pytesseract.image_to_string(Image.open(Imagepath))
+    #print(text[:-1]) test to see if it turns to text and prints accurately
+    translated_text = text[:-1]
+    print (translated_text)
+
+    #This portion will open a txt file and save the image to text variable
+    f = open("image2text.txt","w")
+    f.write(translated_text)
+
+    f = open('image2text.txt')
+
+end_product()
